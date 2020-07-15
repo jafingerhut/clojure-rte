5714e8a64054f9f94d248430980747a51c2a2f26
Jim Newton <jnewton@lrde.epita.fr>
2020-07-15 08:58:52 +0200

    json reader

--- project.clj
+++ project.clj
@@ -27,6 +27,7 @@
   :plugins [      [lein-cloverage "1.1.2"]]
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [lein-cloverage "1.1.2"]
+                 [org.clojure/data.json "1.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]]
   :main ^:skip-aot clojure-rte.core
   :target-path "target/%s"
