bfc2af0097720245e315568852e17f700e367997
Jim Newton <jnewton@lrde.epita.fr>
2020-09-01 09:28:32 +0200

    updated docstring of typep

--- src/clojure_rte/type.clj
+++ src/clojure_rte/type.clj
@@ -28,7 +28,7 @@
   ))
 
 (defmulti typep 
-  "Like instance? except that the arguments are reversed, and the
+  "Like clojure.core/instance? except that the arguments are reversed, and the
   given type designator need not be a class.  The given type 
   designator may be a (1) class, (2) a symbol resolving to a class, or
   (3) a CL style type designator such as
