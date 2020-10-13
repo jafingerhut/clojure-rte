3f9331d1fd8cb86464e27f856133f5c59754004d
Jim Newton <jnewton@marcello.lrde.epita.fr>
2020-10-13 13:00:43 +0200

    added hotspot mode to rte-match

--- src/clojure_rte/api.clj
+++ src/clojure_rte/api.clj
@@ -84,20 +84,56 @@
    multiple conversions/look-ups, as the correspondence of pattern
    to compiled Dfa is maintained via the memoize function."
 
-  (fn [rte _items & {:keys [promise-disjoint]}]
+  (fn [rte _items & {:keys [promise-disjoint
+                            hot-spot]}]
    (dispatch rte 'rte-match)))
 
 (defmethod rte-match :sequential
-  [pattern items & {:keys [promise-disjoint]}]
-  (rte-match (rte-compile pattern) items :promise-disjoint true))
+  [pattern items & {:keys [promise-disjoint
+                           hot-spot]
+                    :as all-keys}]
+  (apply rte-match (rte-compile pattern) items :promise-disjoint true all-keys))
 
 (defmethod rte-match :Dfa
-  [dfa items & {:keys [promise-disjoint]}]
+  [dfa items & {:keys [
+                       ;; if the caller promises that now two transitions in the Dfa
+                       ;;   are labeled with intersecting types, the use
+                       ;;   :promise-disjoint true, in this case rte-match
+                       ;;   can be more efficient and can assume that the
+                       ;;   clauses can be tested in any order.  If the transitions
+                       ;;   are not guaranteed disjoint, then rte-match must
+                       ;;   build new type designators each one containing an and-not
+                       ;;   of the previously seen types. 
+                       promise-disjoint
+                       ;; hot-spot = true -- lazily compile the type checks into Bdds
+                       ;;    which is slow-going but becomes faster the more often you
+                       ;;    re-use the same pattern, either because of loops in the
+                       ;;    Dfa, or when the same Dfa is used to match different
+                       ;;    input sequences.
+                       ;; hot-spot = false -- always interpret the type checks rather
+                       ;;    than converting them to Bdd.  This option is probably faster
+                       ;;    if there are few loops in the Dfa, or if you only use the
+                       ;;    pattern once to check a single input sequence.
+                       hot-spot
+                       ]}]
   (let [state-vec (:states dfa)
         sink-states (set (dfa/find-sink-states dfa))]
     (if (empty? sink-states)
       (rte-match (dfa/extend-with-sink-state dfa) items :promise-disjoint promise-disjoint)
       (let [sink-state-id (:index (first sink-states))]
+        ;; There are two possible transition functions
+        ;;   slow-transition-function -- this is faster if the caller intends to match
+        ;;       the pattern only once.   The pattern is matched by an interpreter,
+        ;;       and it is possible that the same type predicate will be tested multiple
+        ;;       times on the same candidate objects.  If one of the type predicates
+        ;;       is (satisfies slow-predicate) then that slow-predicate may be called
+        ;;       multiple times, resulting in poor performance, especially if the
+        ;;       pattern is used to test multiple sequences.
+        ;;   fast-transition-function -- this is faster if the caller intends to match
+        ;;       the pattern multiple times with different input sequences.  The
+        ;;       pattern is *compiled* into a form where type-designators are converted
+        ;;       to Bdds thus each type check guarantees to never check the same
+        ;;       type predicate multiple times, and sometimes not at all.
         (letfn [(slow-transition-function [transitions]
                   (fn [candidate]
                     (some (fn [[type next-state-index]]
@@ -107,12 +143,16 @@
                           transitions)))
                 (fast-transition-function [transitions]
                   (dfa/optimized-transition-function transitions promise-disjoint sink-state-id))
+                (transition-function [transitions]
+                  (if hot-spot
+                    (fast-transition-function transitions)
+                    (slow-transition-function transitions)))
                 (consume [state-index item]
                   (let [state-obj (state-vec state-index)]
                     (cl/cl-cond
                      ((member state-obj sink-states)
                       (reduced false))
-                     (((fast-transition-function (:transitions state-obj)) item sink-state-id))
+                     (((transition-function (:transitions state-obj)) item sink-state-id))
                      (:else (reduced false)))))]
           (let [final-state (reduce consume 0 items)]
             ;; final-state may be integer desgnating the state which was
